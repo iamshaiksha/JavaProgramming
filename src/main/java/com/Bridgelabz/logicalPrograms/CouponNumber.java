fbe84762fe59fa03fc9181500f487be135ed865a
iamshaiksha <iamshaiksha2019@gmail.com>
2020-01-13 11:02:31 +0530

    [shaiksha] add. CouponNumber program

---
+++ src/main/java/com/Bridgelabz/logicalPrograms/CouponNumber.java
@@ -0,0 +1,40 @@
+package com.Bridgelabz.logicalPrograms;
+ /** generate coupon number
+  *  @Shaik Shaiksha vali
+  *  1.0v
+  *  9/01/2020
+  */
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.Bridgelabz.utility.Utility;
+
+public class CouponNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		ArrayList<Integer> couponList=new ArrayList<>();
+		System.out.println("enter no of coupons");
+		int count=Utility.inputInt();
+		Random random=new Random();
+		for(int i=0;i<count;i++)
+		{
+			int couponNumber=random.nextInt(10000);
+			System.out.println(couponNumber);
+			if(!couponList.contains(couponNumber))
+			{
+				couponList.add(couponNumber);
+			}
+		}
+		for(int i=0;i<couponList.size();i++) {
+			System.out.println("coupon number:"+couponList.get(i));
+
+
+		}
+
+	}
+}
