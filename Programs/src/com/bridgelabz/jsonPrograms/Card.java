b1d8203eee019b9efb0fea684213662cca1e2f43
iamshaiksha <iamshaiksha2019@gmail.com>
2020-01-25 10:50:32 +0530

    [shaiksha] add. DeckOfCardsQueue-Card

---
+++ Programs/src/com/bridgelabz/jsonPrograms/Card.java
@@ -0,0 +1,62 @@
+package com.bridgelabz.jsonPrograms;
+
+public class Card {
+	String suit;
+	String rank;
+	public Card(int suit2,int rank2)
+	{
+
+		this.suit=getSuit(suit2);
+		this.rank=getRank(rank2);
+	}
+
+
+	public String getSuit(int suitNumber) {
+		switch(suitNumber) {
+		case 0:
+			return "Clubs";
+		case 1:
+			return "Diamonds";
+		case 2:
+			return "Hearts";
+		case 3:
+			return "Spades";
+		default:
+			return "";
+		}
+	}
+	public String getRank(int rankNumber) {
+		switch(rankNumber) {
+		case 0:
+			return "2";
+		case 1:
+			return "3";
+		case 2:
+			return "4";
+		case 3:
+			return "5";
+		case 4:
+			return "6";
+		case 5:
+			return "7";
+		case 6:
+			return "8";
+		case 7:
+			return "9";
+		case 8:
+			return "10";
+		case 9:
+			return "Jack";
+		case 10:
+			return "Queen";
+		case 11:
+			return "King";
+		case 12:
+			return "Ace";
+		default:
+			return "";
+		}
+	}
+
+
+}
